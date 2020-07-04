8bddfb22f70e59de310dc2714765963807d84688
mountyz <46027269+mountyz@users.noreply.github.com>
2020-07-04 16:38:34 +0200

    coment, mnist,and stuff

--- gradient.py
+++ gradient.py
@@ -13,35 +13,81 @@ device = torch.device("cpu")
 device = torch.device("cuda:0")
 
 
-def gradient_descent_pytorch_nn():
+def gradient_descent_pytorch_using_nn():
     batch_size, input_dim, hidden_layer_size, output_dim = 64, 1000, 100, 10
 
     x = torch.randn(batch_size, input_dim)
     y = torch.randn(batch_size, output_dim)
 
+    #ici on définit notre réseaud de neuronne. on voit ici une première couche linaire avec les input
+    #un relu et une dernière couche lineair avec les outputs
     model = torch.nn.Sequential(
         nn.Linear(input_dim, hidden_layer_size),
         nn.ReLU(),
         nn.Linear(hidden_layer_size, output_dim),
     )
 
+    #on définit la loss on moyen de la fonction nn qui récupère directement la fonction et son traitement
     loss_fn = nn.MSELoss(reduction='sum')
+    #on définit l'optimizer qui permet de rentre l'update des poids automatique dans un facon spécifique ici Adam mais possible SGD ou autre
     #optimizer_fn = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     for i in range(epochs):
+        #ici nos input rentre dans le model
         y_pred = model(x)
 
+        #on définit la fonction de loss avec le y_pred ainsi que les outputs
         loss = loss_fn(y_pred, y)
 
+        #on met les gradients à 0 avant de faire le backward pass
         model.zero_grad()
 
+        #calcule le gradient de la loss en fonction de tout les parametres possible du model.
+        # à l'interne les paramètres de tous les modules ont le requires_grad=True
+        # donc cette appel calculera le gradients de tout les paramètres du model
         loss.backward()
 
+        #soit on fait manuellement comme précedement
+        # mais la vu que notre model nous retour tout les paramètres on les récupères directement
+        # grace à parametre() et on update les poids en fonctzion du learning rate
         with torch.no_grad():
             for param in model.parameters():
                 param -= learning_rate * param.grad
 
+        #ou alors grace à l'optimizer on effectue notre updates
         #optimizer_fn.step()
+
+def gradient_descent_pytorch_control_over_weigths():
+    batch_size, input_dim, hidden_layer_size, output_dim = 64, 1000, 100, 10
+
+    x = torch.randn(batch_size, input_dim, device=device, dtype=dtype)
+    y = torch.randn(batch_size, output_dim, device=device, dtype=dtype)
+
+    w1 = torch.randn(input_dim, hidden_layer_size, device=device, dtype=dtype, requires_grad=True)
+    w2 = torch.randn(hidden_layer_size, output_dim, device=device, dtype=dtype, requires_grad=True)
+
+    for i in range(epochs):
+        #comme avant définition de y_pred et étant donnée qu'on utilise la fonction backwards pas besoin de stocker les variable comme h ou h_relu
+        y_pred = x.mm(w1).clamp(min=0).mm(w2)
+
+        #défition de la fonction de loss: MSELoss : la somme de la la prediction de y - le vrai y a la puissance 2
+        loss = (y_pred - y).pow(2).sum()
+
+        #ici on utilise l'autograd grace à la fonction backward()
+        # pour que ce la fonctionne il faut spécifié que l'on veut que notre backpropagation se base sur les tensors w1 et w2
+        # et cela en ajoutant un requires_grad=True dans la dénition de notre tensor
+        loss.backward()
+
+        #ici on update manuellement les poids en utilisant la descente du gradient. on wrap dans le torch.no_grad()
+        # parce que on a le requires_grad=True dans le définition de nos poids
+        with torch.no_grad():
+            w1 -= learning_rate * w1.grad
+            w2 -= learning_rate * w2.grad
+
+            # on remet les gradients à 0 après avoir update les poids,
+            # on ne veut pas que les anciens gradients viennent perturber les nouveaux
+            w1.grad.zero_()
+            w2.grad.zero_()
 
 def gradient_descent_pytorch():
     batch_size, input_dim, hidden_layer_size, output_dim = 64, 1000, 100, 10
@@ -52,11 +98,14 @@ def gradient_descent_pytorch():
     w1 = torch.randn(input_dim, hidden_layer_size, device=device, dtype=dtype)
     w2 = torch.randn(hidden_layer_size, output_dim, device=device, dtype=dtype)
 
-    for t in range(epochs):
+    for i in range(epochs):
+        #comme pour numpy sauf que dot ici devient .mm()
         h = x.mm(w1)
+        #comme pour numpy sauf que maximum ici devient .clamp(min=0)
         h_relu = h.clamp(min=0)
         y_pred = h_relu.mm(w2)
 
+        #vulgarisé ?
         #loss = (y_pred - y).pow(2).sum().item()
 
         gradient_y_pred = 2.0 * (y_pred - y)
@@ -79,19 +128,38 @@ def gradient_descent_numpy():
     w2 = np.random.randn(hidden_layer_size, output_dim)
 
     for i in range(epochs):
+        #forward pass pour obtenir y_pred
+        #x.dot(w1) permet de faire une multiplication de matrice entre le vecteur x (qui représente nos inputs)
+        #et le vecteur w1 (qui représente les poids de nos inputs)
         h = x.dot(w1)
+        #np.maximum(h, 0) on récupère une array contenant tout les éléments supérieur à 0
         h_relu = np.maximum(h, 0)
+        #h_relu.dot(w2) multiplication de matrice entre vecteur h_relu(tous les éléments positif de la première mulpication matriciel entre x et w1)
+        # et le vecteur w2 (qui représente les poids des outputs)
         y_pred = h_relu.dot(w2)
+        #x->w1->relu->w2->y_pred
 
+        #définition de la loss MSELoss: Mean Square Error Loss besoin de vulgariser ?
+        #la somme de y_pred - y au carré
         #loss = np.square(y_pred - y).sum()
 
+        #Back propagation en considération de la loss(ici on utilise pas la définition de la loss mais on la fait nous)
+        #2(y_pred-y) equation MSELoss
         gradient_y_pred = 2.0 * (y_pred - y)
+        #calcul du gradient de w2 avec h_relu avec le gradient du y_pred
+        #ici le .T permet de transposer un vecteur 2 dimension
         gradient_w2 = h_relu.T.dot(gradient_y_pred)
+        #calcul du gradient de h_relu avec le gradient du y_pred et w2
         gradient_h_relu = gradient_y_pred.dot(w2.T)
+        #copie du gradient h relu pour le gradient_h
         gradient_h = gradient_h_relu.copy()
+        #dans le gradient_h, tous ce qui est inférieur à 0 dans le h est mis à 0
+        #lacune je comprend pas
         gradient_h[h < 0] = 0
+        #calcul du gradient_w1 avec x et gradient_h
         gradient_w1 = x.T.dot(gradient_h)
 
+        #update des poids en fonction du learning_rate trop mettre fait déborder l'apprentissage devient alors un surentrainement
         w1 -= learning_rate * gradient_w1
         w2 -= learning_rate * gradient_w2
 
@@ -105,8 +173,13 @@ def linear_regression_nothing():
 
     while 1:
         for i in range(epochs):
+            #représente notre model de prediction, formule mathématique
             prediction = m * X + b
+
+            #l'erreur c'est la prediction moins l'output
             error = prediction - Y
+
+            #on met à jour notre model en fonction du learning rate
             m = m - learning_rate * (error * X)
             b = b - learning_rate * error
 
--- mnist_pytorch.py
+++ mnist_pytorch.py
@@ -1,21 +1,118 @@
+import pickle
+import gzip
+import requests
 import torch
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+import torch.nn as nn
+import math
+from pathlib import Path
+from matplotlib import pyplot
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+import numpy as np
 
-root = './data'
+#a revoir je devrais faire en sortes d'être utilisable partout
+#ici utilisation de pytorch pour télécharger mnist et avoir le dataset
+data_path = Path("M:/users/Documents/data")
+path = data_path / "mnist"
 
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-# if not exist, download mnist dataset
-train_set = datasets.MNIST(root=root, train=True, transform=trans, download=True)
-test_set = datasets.MNIST(root=root, train=False, transform=trans, download=True)
+path.mkdir(parents=True, exist_ok=True)
+
+url = "http://deeplearning.net/data/mnist/"
+filename = "mnist.pkl.gz"
+
+if not (path / filename).exists():
+        content = requests.get(url + filename).content
+        (path / filename).open("wb").write(content)
+
+with gzip.open((path / filename).as_posix(), "rb") as f:
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
+
+#pyplot.imshow(x_train[0].reshape((28, 28)), cmap="gray")
+
+#_________________________________________________________________________________________________________________
+
+#le dataset récupéré est en numpy il faut alors convert nos datas en tensor
+x_train, y_train, x_valid, y_valid = map(torch.tensor, (x_train, y_train, x_valid, y_valid))
+
+#récupère la taille des données
+n, c = x_train.shape
+
+#initialisation des poids, 784 car l'image est en 28x28 en divisant par la racine carré de 784
+weights = torch.randn(784, 10) / math.sqrt(784)
+
+#ici on ajouter le requires_grad sur le tensor des weigths
+weights.requires_grad_()
+
+#on créer notre bias (besoin de plus d'inforamtion)
+bias = torch.zeros(10, requires_grad=True)
+
+#softmax permet de transformer dans input (0 à 9) en probabilité en prenant l'exponientiel
+# et en la divisant par la somme de toutes les autres exponentielles
+def log_softmax(x):
+    return x - x.exp().sum(-1).log().unsqueeze(-1)
+
+#ici on a notre modèle qui prend le sample en cours et le softmax en utilisant les poids
+#le @ fait le dot
+def model(x):
+    return log_softmax(x @ weights + bias)
+
+#Pas sur du  fonctionnement besoin d'explication
+def loss_function_cross_entropy(pred, target):
+    return target * math.log(pred)
+
+def accuracy(out, y):
+    preds = torch.argmax(out, dim=1)
+    return (preds == y).float().mean()
 
 batch_size = 64
+criterion = nn.CrossEntropyLoss()
+learning_rate = 0.5
+epochs = 10
+loss_track = []
+accuracy_track = []
 
-train_loader = torch.utils.data.DataLoader(
-                 dataset=train_set,
-                 batch_size=batch_size,
-                 shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-                dataset=test_set,
-                batch_size=batch_size,
-                shuffle=False)
+for epoch in range(epochs):
+    for i in range((n - 1)// batch_size + 1):
+        #definition du debut de l'echantillion
+        start = i * batch_size
+
+        #definition du fin de l'echantillion
+        end = start + batch_size
+
+        #recupération des données en fonction de l'échantillion
+        x = x_train[start:end]
+        y = y_train[start:end]
+
+        #calcule du y_pred avec le modèle (softmax)
+        y_pred = model(x)
+
+        #calcule de la loss avec le y_pred et le y
+        loss = criterion(y_pred, y)
+        #print(loss, accuracy(y_pred, y))
+        #print(accuracy(y_pred, y))
+        #print(loss.detach().numpy())
+        if i % 100 == 0:
+            loss_track.append(loss.item())
+            accuracy_track.append(accuracy(y_pred, y))
+
+        #backpropagation
+        loss.backward()
+
+        #update manuel classic avec pytorch
+        with torch.no_grad():
+            weights -= weights.grad * learning_rate
+            bias -= bias.grad * learning_rate
+            weights.grad.zero_()
+            bias.grad.zero_()
+
+pyplot.plot(loss_track)
+pyplot.show()
+pyplot.plot(accuracy_track)
+pyplot.show()
+
+
+
+
+
+
+
